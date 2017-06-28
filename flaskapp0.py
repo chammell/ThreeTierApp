d3ee5fea499bf7feee363ff93fa8937b5e087a3b
chammell <chammell@charleshammell.com>
2017-06-28 14:37:56 -0400

    backup of original flaskapp

---
+++ flaskapp0.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from collections import Counter
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+	return 'Hello from flaskapp!'
+
+@app.route('/countme/<input_str>')
+def count_me(input_str):
+	input_counter = Counter(input_str)
+	response = []
+	for letter, count in input_counter.most_common():
+		response.append('"{}": {}'.format(letter, count))	
+	return '<br>'.join(response)
+
+if __name__ == '__main__':
+	app.run()
