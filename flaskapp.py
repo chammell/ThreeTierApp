31123eac1e067782172f23e40ddef45414e04e7f
chammell <chammell@charleshammell.com>
2017-06-29 11:52:05 -0400

    updating file to get in sync

--- flaskapp.py
+++ flaskapp.py
@@ -1,19 +1,19 @@
 from flask import Flask
-from collections import Counter
+from flask.ext.mysqldb import MySQL
 
 app = Flask(__name__)
+app.config['MYSQL_HOST'] = 'acloudguru.cqsq6q6oma7l.us-east-1.rds.amazonaws.com'
+app.config['MYSQL_USER'] = 'acloudguru'
+app.config['MYSQL_PASSWORD'] = 'cbh12345'
+app.config['MYSQL_DB'] = 'testdb'
+mysql = MySQL(app)
 
 @app.route('/')
-def hello_world():
-	return 'Hello from flaskapp!'
-
-@app.route('/countme/<input_str>')
-def count_me(input_str):
-	input_counter = Counter(input_str)
-	response = []
-	for letter, count in input_counter.most_common():
-		response.append('"{}": {}'.format(letter, count))	
-	return '<br>'.join(response)
+def index():
+	cur = mysql.connection.cursor()
+	cur.execute('''SELECT data FROM example WHERE id = 1''')
+	rv = cur.fetchall()
+	return str(rv)
 
 if __name__ == '__main__':
-	app.run()
+app.run(debug=True)
